2f1e47a0c61e0f90081ca976bbd7d6db0fb20bce
Sriraj Bura <sriraj.bura16@gmail.com>
2021-04-21 16:14:52 -0400

    implemented addeventtocell function

--- index.js
+++ index.js
@@ -2,16 +2,26 @@
 const container = document.querySelector('#cell-container')
 
 //---------
+
 const gameBoard = (function(){
 
   let board = ['X', 'O', 'X', 'O', 'X', 'X', 'X', 'O', 'O']
 
   const render = () => {
-    board.forEach(ele => {
+    board.forEach((ele, index) => {
       const cell = document.createElement('div')
       cell.classList.add('cell')
-      cell.textContent = ele
+      cell.setAttribute('data-index', index)
       container.appendChild(cell)
+      _addEventToCell(cell)
+    })
+  }
+
+  const _addEventToCell = (cell) => {
+    cell.addEventListener('click', () => {
+      let index = cell.dataset.index
+      console.log(index)
+      return index
     })
   }
 
@@ -19,3 +29,7 @@ const gameBoard = (function(){
 })()
 
 gameBoard.render()
+
+console.log(container.childNodes)
+
+console.log(gameBoard.full(['3', '3']))
